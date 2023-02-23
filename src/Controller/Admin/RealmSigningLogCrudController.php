210a77abb1b14586ccf46c2a1dd8fd9ea3bf3e8a
shaky212 <sjaak@ibuildings.nl>
2023-02-23 10:46:05 +0100

    Hide pseudo account and dash nullable values

--- src/Controller/Admin/RealmSigningLogCrudController.php
+++ src/Controller/Admin/RealmSigningLogCrudController.php
@@ -3,31 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\RealmSigningLog;
-use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\BatchActionDto;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterCrudActionEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeCrudActionEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityDeletedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Exception\EntityRemoveException;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\InsufficientEntityPermissionException;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 
 class RealmSigningLogCrudController extends AbstractCrudController
 {
@@ -48,8 +40,10 @@ class RealmSigningLogCrudController extends AbstractCrudController
             IdField::new('serial'),
             TextField::new('requester'),
             TextField::new('subjectWithoutCustomerName')
-                ->setLabel('Pseudo account'),
+                ->setLabel('Pseudo account')
+                ->hideOnIndex(),
             TextField::new('realm'),
+            TextField::new('client'),
             DateTimeField::new('issued')
                 ->setFormat('yyyy-MM-dd HH:mm:ss')
                 ->formatValue(function ($value, $entity) {
@@ -72,7 +66,6 @@ class RealmSigningLogCrudController extends AbstractCrudController
                     'data-bs-toggle' => 'modal',
                     'data-bs-target' => '#modal-confirm',
                 ]);
-
 
         return parent::configureActions($actions)
             ->disable(Action::EDIT)
@@ -97,56 +90,15 @@ class RealmSigningLogCrudController extends AbstractCrudController
     public function configureFilters(Filters $filters): Filters
     {
         return parent::configureFilters($filters)
-            ->add('revoked');
+            ->add(TextFilter::new('realm'))
+            ->add(DateTimeFilter::new('revoked'));
     }
 
-//    public function createIndexQueryBuilder(
-//        SearchDto $searchDto,
-//        EntityDto $entityDto,
-//        FieldCollection $fields,
-//        FilterCollection $filters): QueryBuilder
-//    {
-//        $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
-//
-//        // if user defined sort is not set
-//        if (0 === count($searchDto->getSort())) {
-//            $queryBuilder
-//                ->addSelect('REPLACE(sub, \'CN=\', \'\') AS HIDDEN subject_without_customer_name');
-//        }
-//
-//        return $queryBuilder;
-//    }
-
+    /*
+     * LinkToCrudAction to revoke one realm signing log, configured at configureActions
+     */
     public function revokeRealmSigningLog(AdminContext $context) : Response
     {
-        $event = new BeforeCrudActionEvent($context);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-//        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::EDIT, 'entity' => $context->getEntity()])) {
-//            throw new ForbiddenActionException($context);
-//        }
-
-        if (!$context->getEntity()->isAccessible()) {
-            throw new InsufficientEntityPermissionException($context);
-        }
-
-        $csrfToken = $context->getRequest()->request->get('token');
-//        if ($this->container->has('security.csrf.token_manager') && !$this->isCsrfTokenValid('ea-delete', $csrfToken)) {
-//            return $this->redirectToRoute($context->getDashboardRouteName());
-//        }
-
-        $entityInstance = $context->getEntity()->getInstance();
-
-        $event = new BeforeEntityDeletedEvent($entityInstance);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-        $entityInstance = $event->getEntityInstance();
-
         try {
             $entity = $context->getEntity()->getInstance();
             $entityManager = $this->container->get('doctrine')->getManagerForClass(RealmSigningLog::class);
@@ -156,22 +108,15 @@ class RealmSigningLogCrudController extends AbstractCrudController
             throw new EntityRemoveException(['entity_name' => $context->getEntity()->getName(), 'message' => $e->getMessage()]);
         }
 
-        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
-            'entity' => $context->getEntity(),
-        ]));
-
-        $event = new AfterCrudActionEvent($context, $responseParameters);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
         $url = $context->getReferrer()
             ?? $this->container->get(AdminUrlGenerator::class)->setAction(Action::INDEX)->generateUrl();
 
         return $this->redirect($url);
     }
 
+    /*
+     * LinkToCrudAction to revoke multiple (batch) realm signing logs, configured at configureActions
+     */
     public function revokeRealmSigningLogBatch(BatchActionDto $batchActionDto)
     {
         $className = $batchActionDto->getEntityFqcn();
@@ -186,5 +131,4 @@ class RealmSigningLogCrudController extends AbstractCrudController
 
         return $this->redirect($batchActionDto->getReferrerUrl());
     }
-
 }
